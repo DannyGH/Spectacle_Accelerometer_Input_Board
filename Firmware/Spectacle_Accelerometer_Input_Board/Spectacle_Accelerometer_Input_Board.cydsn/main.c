6097ebd3fc7ae965e27c90f786308e228036cf44
Mike Hord <mike.hord@sparkfun.com>
2016-10-12 15:51:09 -0600

    Nice place for a code commit. Has structure of I2C registers in place, along with necessary handlers and updates for new styles.

--- Firmware/Spectacle_Accelerometer_Input_Board/Spectacle_Accelerometer_Input_Board.cydsn/main.c
+++ Firmware/Spectacle_Accelerometer_Input_Board/Spectacle_Accelerometer_Input_Board.cydsn/main.c
@@ -1,14 +1,3 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include <project.h>
 #include <stdbool.h>
 #include <math.h>
@@ -16,18 +5,18 @@
 #include <stdio.h>
 #include "ADXL345.h"
 
-#define I2C_BUFFER_SIZE 37
-#define I2C_BUFFER_RW_BOUNDARY 37
+#define I2C_BUFFER_SIZE 16
+#define I2C_BUFFER_RW_BOUNDARY 16
 
 volatile int32 systemTimer = 0;
 
 CY_ISR_PROTO(tickISR);
 
+uint8 registerSpace[I2C_BUFFER_SIZE];
 int main()
 {
   CyGlobalIntEnable; /* Enable global interrupts. */
   
-  uint8 registerSpace[I2C_BUFFER_SIZE];
   EZI2C_Start();
   EZI2C_EzI2CSetBuffer1(I2C_BUFFER_SIZE, I2C_BUFFER_RW_BOUNDARY, registerSpace);
   
@@ -45,12 +34,35 @@ int main()
   int i = 0;
   char buffer[64];
   
-  int16 xMag = 0;
-  int16 yMag = 0;
-  int16 zMag = 0;
-  int16 maxAxis;
-  uint8 *active;
-  active = &registerSpace[0];
+  uint8 *boardID = 0;
+  uint8 *boardType = 0;
+  uint8 *change = 0;
+  int16 *xMag = 0;
+  int16 *yMag = 0;
+  int16 *zMag = 0;
+  uint8 *maxAxis = 0;
+  int16 *magnitude = 0;
+  uint8 *active = 0;
+  boardID = &registerSpace[0];
+  boardType = &registerSpace[1];
+  change = &registerSpace[2];
+  active = &registerSpace[3];
+  maxAxis = &registerSpace[4];
+  magnitude = (int16*)&registerSpace[5];
+  xMag = (int16*)&registerSpace[7];
+  yMag = (int16*)&registerSpace[9];
+  zMag = (int16*)&registerSpace[11];
+
+  *boardID = 0xa3;
+  *boardType = 1;
+  *change = 0;
+  *active = 0;
+  *maxAxis = 0;
+  *magnitude = 0;
+  *xMag = 0;
+  *yMag = 0;
+  *zMag = 0;
+
   
   UART_UartPutString("Hello world");
   write8Bits(POWER_CTL, 0x08);
@@ -79,44 +91,44 @@ int main()
       }
       if (INT1_Read() != 0)
       {
-        UART_UartPutString("INACTIVE\n");
+        //UART_UartPutString("INACTIVE\n");
         read8Bits(INT_SOURCE, &temp);
         *active = 0;
       }
       if (INT2_Read() != 0)
       {
-        UART_UartPutString("ACTIVE\n");
+        //UART_UartPutString("ACTIVE\n");
         read8Bits(INT_SOURCE, &temp);
         *active = 1;
       }
-      readXYZ(&xMag, &yMag, &zMag);
-      int16 xAbs = abs(xMag);
-      int16 yAbs = abs(yMag);
-      int16 zAbs = abs(zMag); 
+      readXYZ(xMag, yMag, zMag);
+      int16 xAbs = abs(*xMag);
+      int16 yAbs = abs(*yMag);
+      int16 zAbs = abs(*zMag); 
       if (xAbs > yAbs)
       {
         if (zAbs > xAbs) 
         {
-          if (zMag < 0) maxAxis = Botup;
-          else          maxAxis = Topup;
+          if (zMag < 0) *maxAxis = Botup;
+          else          *maxAxis = Topup;
         }
         else
         {
-          if (xMag < 0) maxAxis = Dup;
-          else          maxAxis = Bup;
+          if (xMag < 0) *maxAxis = Dup;
+          else          *maxAxis = Bup;
         }
       }
       else
       {
         if (zAbs > yAbs) 
         {
-          if (zMag < 0) maxAxis = Botup;
-          else          maxAxis = Topup;
+          if (zMag < 0) *maxAxis = Botup;
+          else          *maxAxis = Topup;
         }
         else
         {
-          if (yMag < 0) maxAxis = Bup;
-          else          maxAxis = Aup;
+          if (yMag < 0) *maxAxis = Bup;
+          else          *maxAxis = Aup;
         }
       }
     }
@@ -133,15 +145,16 @@ int main()
       {
         LED_Write(1);
       }
-      readXYZ(&xMag, &yMag, &zMag);
-      float vectorMagnitude = sqrt((float)xMag*(float)xMag + 
-                                   (float)yMag*(float)yMag +
-                                   (float)zMag*(float)zMag);
+      readXYZ(xMag, yMag, zMag);
+      float vectorMagnitude = sqrt((float)*xMag*(float)*xMag + 
+                                   (float)*yMag*(float)*yMag +
+                                   (float)*zMag*(float)*zMag);
+      *magnitude = (int)vectorMagnitude;
       sprintf(buffer, "%d\n", (int)vectorMagnitude);
       UART_UartPutString(buffer);
-      sprintf(buffer, "X: %d Y: %d Z: %d\n", xMag, yMag, zMag);
+      sprintf(buffer, "X: %d Y: %d Z: %d\n", *xMag, *yMag, *zMag);
       UART_UartPutString(buffer);
-      switch (maxAxis)
+      switch (*maxAxis)
       {
         case Aup:
           sprintf(buffer, "A edge is up\n");
@@ -167,48 +180,39 @@ int main()
   }
 }
 
-bool readXYZ(int16 *x, int16 *y, int16 *z)
+void readXYZ(int16 *x, int16 *y, int16 *z)
 {
   read16Bits(DATAX0, x);
   read16Bits(DATAY0, y);
   read16Bits(DATAZ0, z);
+}
+
+bool read16Bits(uint8 address, int16 *read)
+{
+  I2C_I2CMasterWriteBuf(ADXL345_ADDR, &address, 1, I2C_I2C_MODE_COMPLETE_XFER);
+  while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_WR_CMPLT));
+  I2C_I2CMasterReadBuf(ADXL345_ADDR, (uint8*)read, 2, I2C_I2C_MODE_COMPLETE_XFER);
+  while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_RD_CMPLT));
   return true;
 }
 
-bool read16Bits(int address, int16 *read)
+bool read8Bits(uint8 address, uint8 *read)
 {
-  I2C_I2CMasterClearStatus();
-  I2C_I2CMasterSendStart(ADXL345_ADDR, I2C_I2C_WRITE_XFER_MODE);
-  I2C_I2CMasterWriteByte(address);
-  I2C_I2CMasterSendStop();
-  I2C_I2CMasterSendStart(ADXL345_ADDR, I2C_I2C_READ_XFER_MODE);
-  *read = I2C_I2CMasterReadByte(I2C_I2C_ACK_DATA);
-  *read |= I2C_I2CMasterReadByte(I2C_I2C_NAK_DATA)<< 8;
-  I2C_I2CMasterSendStop();
+  I2C_I2CMasterWriteBuf(ADXL345_ADDR, &address, 1, I2C_I2C_MODE_COMPLETE_XFER);
+  while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_WR_CMPLT));
+  I2C_I2CMasterReadBuf(ADXL345_ADDR, (uint8*)read, 1, I2C_I2C_MODE_COMPLETE_XFER);
+  while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_RD_CMPLT));
   return true;
 }
 
-bool read8Bits(int address, uint8 *read)
+bool write8Bits(uint8 address, uint8 write)
 {
-  I2C_I2CMasterClearStatus();
-  I2C_I2CMasterSendStart(ADXL345_ADDR, I2C_I2C_WRITE_XFER_MODE);
-  I2C_I2CMasterWriteByte(address);
-  I2C_I2CMasterSendStop();
-  I2C_I2CMasterSendStart(ADXL345_ADDR, I2C_I2C_READ_XFER_MODE);
-  *read = I2C_I2CMasterReadByte(I2C_I2C_NAK_DATA);
-  I2C_I2CMasterSendStop();
-  return true;
-}
 
-bool write8Bits(int address, uint8 write)
-{
-  I2C_I2CMasterClearStatus();
-  bool retVal = false; // Assume a failed read.
-  if ( I2C_I2CMasterSendStart(ADXL345_ADDR, I2C_I2C_WRITE_XFER_MODE) !=
-       I2C_I2C_MSTR_NO_ERROR) return retVal;
-  if ( I2C_I2CMasterWriteByte(address) != I2C_I2C_MSTR_NO_ERROR) return retVal;
-  I2C_I2CMasterWriteByte(write);
-  if ( I2C_I2CMasterSendStop() != I2C_I2C_MSTR_NO_ERROR) return retVal;
+  uint8 temp[2];
+  temp[0] = address;
+  temp[1] = write;
+  I2C_I2CMasterWriteBuf(ADXL345_ADDR, temp, 2, I2C_I2C_MODE_COMPLETE_XFER);
+  while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_WR_CMPLT));
   return true;
 }
 
