d4dd958e1787d8bec1c1d59047d356b473d8989a
Mike Hord <mike.hord@sparkfun.com>
2016-10-31 11:36:04 -0600

    Change 2.2k I2C pullup to 4.7k Add Arduino test sketch

---
+++ Firmware/Spectacle_Accelerometer_Arduino_test_sketch/Spectacle_Accelerometer_Arduino_test_sketch.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+
+void setup() 
+{
+  pinMode(7, OUTPUT);
+  Wire.begin();
+}
+
+void loop() 
+{
+  if (isActive())
+  {
+    digitalWrite(7, HIGH);
+  }
+  else
+  {
+    digitalWrite(7, LOW);
+  }
+}
+
+boolean isActive()
+{
+  Wire.beginTransmission(0x09);
+  Wire.write(0);
+  Wire.endTransmission();
+  Wire.requestFrom(0x09, 1);
+  if (Wire.read())
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
